e0adba5c7540289bd0f61db918e046cc5b321dfe
Unknown <ali@faradji.se>
2017-03-15 13:33:23 +0100

    Added a event handler that writes to inputfield

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
- $(function() {
-    console.log( "DOM ready!" );
+$(function() {
+    console.log("DOM ready!");
+
+    document.getElementById('#area').addEventListener('click', function(event) {
+        event.preventDefault();
+        writeToInputField(event.target.id);
+    });
+
 });
+
+function writeToInputField(elementId) {
+    $(document).find("#input-field").text(elementId);
+}
