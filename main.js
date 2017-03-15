cff1f85c2669e685c4f49e43db4dd3dd6cdd7319
Unknown <ali@faradji.se>
2017-03-15 13:36:00 +0100

    update

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(function() {
     console.log("DOM ready!");
 
-    document.getElementById('#area').addEventListener('click', function(event) {
+    document.getElementById('.calculator').addEventListener('click', function(event) {
         event.preventDefault();
         writeToInputField(event.target.id);
     });
