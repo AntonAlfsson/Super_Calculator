282c3c602f2db33826ad1c45ee085c528bf60834
AntonAlfsson <Anton.Alfsson@gmail.com>
2017-03-20 10:46:48 +0100

    Task delatMed klar

--- main.js
+++ main.js
@@ -20,5 +20,9 @@ function writeToInputField(elementId) {
     $(document).find("#input-field").val(text);
  
 }
+
+function division(){ // funktion för division
+    savedValue = this.savedValue/$(document).find("#input-field").val();
+}
  
 
