dc046b9df65a40bfcbca0b5e21f82f63ca9520e5
Alexiz Knöös <qwastanja@hotmail.com>
2017-03-20 15:15:27 +0100

    fixat så att rutan rensas

--- main.js
+++ main.js
@@ -3,14 +3,15 @@ $(document).on("click", "button", function() {
     if(parseInt($(this).attr("data-id")) || $(this).attr("data-id") == 0){
         console.log($(this).attr("data-id"));
         writeToInputField($(this).attr("data-id"));
-    }else{
-        console.log('hej');
-        console.log($(this).attr("data-id"));
-        
-    }
-    
-});
+    }else if(($(this).attr("data-id")) == 'ac'){
+       erase();
 
+    }
+    else{
+        console.log('hej');
+        console.log($(this).attr("data-id"));        
+    }    
+});
 
 function writeToInputField(elementId) {
     
@@ -18,7 +19,9 @@ function writeToInputField(elementId) {
     text += elementId;
     
     $(document).find("#input-field").val(text);
- 
 }
- 
 
+function erase(){
+
+    $(document).find("#input-field").val('');
+}
